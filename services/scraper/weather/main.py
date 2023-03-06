e663ae0fbaa9ddf644ccc786f686e7d6b9b27327
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-03-06 12:30:31 +0330

    first commit

---
+++ services/scraper/weather/main.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+# enter city name
+city = "lucknow"
+
+# create url
+url = "https://www.google.com/search?q="+"weather"+city
+
+# requests instance
+html = requests.get(url).content
+
+# getting raw data
+soup = BeautifulSoup(html, 'html.parser')
+
+# get the temperature
+temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+
+# this contains time and sky description
+str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+
+# format the data
+data = str.split('\n')
+time = data[0]
+sky = data[1]
+
+# list having all div tags having particular class name
+listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+
+# particular list with required data
+strd = listdiv[5].text
+
+# formatting the string
+pos = strd.find('Wind')
+other_data = strd[pos:]
+
+# printing all the data
+print("Temperature is", temp)
+print("Time: ", time)
+print("Sky Description: ", sky)
+print(other_data)
